37966a33e51cb5ca2cefe386510c07281bda497d
JoseLArantes <jlarantes@gmail.com>
2015-03-13 15:27:38 -0300

    modified:   src/ime/lists/ListaArrayObjects.java

--- src/ime/lists/ListaArrayObjects.java
+++ src/ime/lists/ListaArrayObjects.java
@@ -1,5 +1,37 @@
 package ime.lists;
 
-public class ListaArrayObjects {
+//Lista de Objetos ALUNOS
+public class Alunos{
+	private String nome;
+	private int idade;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 
+	
+	public String toString(){
+		return this.nome;
+		
+	}
+	
+}
+
+
+public class ListaArrayObjects {
+	private Object[] lista;
+	
+	
+	
+	
+	
+	
 }
